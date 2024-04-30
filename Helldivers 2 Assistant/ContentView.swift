42dbde7b33464165a021ccaa9217467c3b94e75e
Omay <70179858+Omay238@users.noreply.github.com>
2024-04-30 12:07:02 -0600

    Initial Commit

---
+++ Helldivers 2 Assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Helldivers 2 Assistant
+//
+//  Created by Leonard Maculo on 4/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Helldivers 2 Assistant/Helldivers_2_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Helldivers_2_AssistantApp.swift
+//  Helldivers 2 Assistant
+//
+//  Created by Leonard Maculo on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Helldivers_2_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
